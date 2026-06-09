1a94a63974869c1d623f134998f0b9a767d33703
Maia Wichman <maiawichman@gmail.com>
2026-06-08 21:09:33 -0400

    added embedding functions

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # RAG
 Retrieval-Augmented Generation (RAG) is a technique to augment large language models (LLMs) with new information from selected datasources (typically unstructure data). This allows LLMs to reference source documents and provide more accurate and contex-aware responses to user queries. 
 
+## Requirements
+
+- Python 3.12+
+- [Ollama](https://ollama.com/download) installed and running
+- Install requirements.txt
+- Pull `embeddinggemma` model (TODO: Can I add this to the requirements.txt somehow?)
+    ```
+    ollama pull embeddinggemma
+    ```
+
 ## Data Preparation
+
+## Future Features
+- Semantic chunking (on header, paragraph, complete thought, etc) rather than by character length.
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pypdf import PdfReader
 import os
-
+import ollama
 
 def _read_pdfs(dir_path) -> dict:
     """
@@ -8,11 +8,12 @@ def _read_pdfs(dir_path) -> dict:
     
     params
     -----
-    dir_path: str - The path to the directory to search for PDF files.
+    dir_path: str 
+        The path to the directory to search for PDF files.
 
     returns
     -----
-    dict - A dictionary where keys are PDF file names and values are lists of text from each page.
+    dict: A dictionary where keys are PDF file names and values are lists of text from each page.
     """
     paths = os.listdir(dir_path)
     pdf_names = [f for f in paths if f.endswith('.pdf')]
@@ -29,4 +30,46 @@ def _read_pdfs(dir_path) -> dict:
     return pdf_pages
 
 
+def _embed(text: str) -> list:
+    """
+    Embed text using the Ollama embedding model.
+    
+    params
+    -----
+    text: str
+        The text to be embedded.
 
+    return 
+    -----
+    list: A list of the embedding for the input text.
+    """
+    embedding = ollama.embed(
+        model='embeddinggemma',
+        input=text
+    )
+
+    return embedding['embeddings']
+
+
+def embed_batch(texts: list) -> list:
+    """
+    Embed a batch of texts using the Ollama embedding model.
+    
+    params
+    -----
+    texts: list
+        A list of strings to be embedded.
+
+    return 
+    -----
+    list: A list of embeddings for the input texts.
+    """
+    batch = ollama.embed(
+        model='embeddinggemma',
+        input=texts
+    )
+
+    return batch['embeddings']
+
+if __name__ == "__main__":
+    pass
